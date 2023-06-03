8ff0418e4bacfd52c9f0726c11f86689721e0779
Sandro Santos <sandro.dev@gmail.com>
2023-06-03 16:32:01 -0300

    feat: :sparkles: changes address from string to value object and add new methods

--- src/domain/entities/customer.spec.ts
+++ src/domain/entities/customer.spec.ts
@@ -1,30 +1,47 @@
+import Address from "../value-objects/address.vo"
 import Customer from "./customer"
 
 describe("Entity Unit Test", () => {
 
   it('should be able to create a customer', () => {
-    const customer = new Customer("John", "john@doe.com")
+    const customer = new Customer("John", "john@test.com")
 
     expect(customer).toBeDefined()
     expect(customer.id).toBeDefined()
     expect(customer.name).toBe("John")
-    expect(customer.email).toBe("john@doe.com")
+    expect(customer.email).toBe("john@test.com")
     expect(customer.address).toBeUndefined()
     expect(customer.is_active).toBe(false)
     expect(customer.created_at).toBeInstanceOf(Date)
+  })
+  it('should be able to change a customer name or email', () => {
+    const customer = new Customer("John", "john@test.com")
+
+    expect(customer).toBeDefined()
+    expect(customer.id).toBeDefined()
+    expect(customer.name).toBe("John")
+
+    customer.changeName("Bill")
+    expect(customer.name).toBe("Bill")
+
+    customer.changeEmail("bill@test.com")
+    expect(customer.email).toBe("bill@test.com")
+
   })
 
 
   it('should NOT be able to create a customer with INVALID parameters', () => {
 
-    expect(() => new Customer("", "john@doe.com")).toThrowError("name is required")
+    expect(() => new Customer("", "john@test.com")).toThrowError("name is required")
 
     expect(() => new Customer("Johnm", "")).toThrowError("email is required")
 
   })
   it('should be able to activate a customer', () => {
 
-    const customer = new Customer("John", "john@doe.com", "Street A, Number 123, City A, State A, Brazil")
+    const customer = new Customer("John", "john@test.com")
+    const address = new Address("Street A", 123, "City A", "State A", "Brazil")
+    customer.changeAddress(address)
     customer.activate()
 
     expect(customer).toBeDefined()
@@ -34,7 +51,9 @@ describe("Entity Unit Test", () => {
 
   it('should be able to deactivate a customer', () => {
 
-    const customer = new Customer("John", "john@doe.com", "Street A, Number 123, City A, State A, Brazil")
+    const customer = new Customer("John", "john@test.com")
+    const address = new Address("Street A", 123, "City A", "State A", "Brazil")
+    customer.changeAddress(address)
     customer.activate()
 
     expect(customer).toBeDefined()
@@ -48,7 +67,7 @@ describe("Entity Unit Test", () => {
 
   it('should NOT be able to activate a customer without an addres', () => {
 
-    const customer = new Customer("John", "john@doe.com")
+    const customer = new Customer("John", "john@test.com")
     expect(() => {
       customer.activate()
     }).toThrowError("address is required to activate a customer")
